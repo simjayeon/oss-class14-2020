37e8fc8ca4865e8b23e6ea86d9f6fa7e9a3053e5
unknown <simjayeon98@gmail.com>
2020-04-13 13:41:46 +0900

    Add add() function

--- hello.c
+++ hello.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+int add(int a, int b)
+{
+	return a+b;
+}
 void swap(int* a, int* b)
 {
 	int temp = *a;
 	*a =b*;
+	*b = temp;
+	return;
+}
 
 int main(){
 	printf("hello!!!!!!!!!!\n");
+
+	int a=3, b=2;
+	printf("a+b=%d\n", add(a,b));
+
 	return 0;
 }
